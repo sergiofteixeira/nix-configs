68056f50c8484e14db5c7c1e1456903c41a32339
Sergio Teixeira <sergiofpteixeira@gmail.com>
2023-08-14 17:30:53 +0100

    cleanup

--- helios/configuration.nix
+++ helios/configuration.nix
@@ -11,26 +11,14 @@
       (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "helios"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "helios";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Lisbon";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_PT.UTF-8";
     LC_IDENTIFICATION = "pt_PT.UTF-8";
@@ -43,7 +31,6 @@
     LC_TIME = "pt_PT.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
@@ -56,7 +43,6 @@
 
   services.vscode-server.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.steixeira = {
     isNormalUser = true;
     description = "Sergio Teixeira";
