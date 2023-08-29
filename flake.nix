62639e8e0fcc9abbda10cb6e9fc55f8e0137474e
sergiofteixeira <sergiofpteixeira@gmail.com>
2023-08-29 10:45:05 +0100

    add home manager initial

--- flake.nix
+++ flake.nix
@@ -4,23 +4,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = all@{ self, nixpkgs, vscode-server, ... }: {
+  outputs = all@{ self, nixpkgs, vscode-server, home-manager, ... }: {
 
     nixosConfigurations = {
       # sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>
-      "helios" = nixpkgs.lib.nixosSystem {
+      helios = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         modules = [
           vscode-server.nixosModules.default
           ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
           ./helios/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.steixeira = import ./helios/home.nix;
+          }
         ];
       };
 
-      "phrike" = nixpkgs.lib.nixosSystem {
+      phrike = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         modules = [
