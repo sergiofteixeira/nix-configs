db79e448780aa17564f3ef1d1db1c32b8bb6225e
Sergio Teixeira <sergiofpteixeira@gmail.com>
2023-08-29 11:51:39 +0100

    rename modules

--- helios/configuration.nix
+++ helios/configuration.nix
@@ -6,8 +6,8 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../modules/traefik.nix
-    ../modules/grafana.nix
-    ../modules/prometheus.nix
+    ../modules/monitoring/grafana.nix
+    ../modules/monitoring/prometheus.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
