79a6e6e64ef2c1f4ae4fb72c2bb174c25fb7e29d
Sergio Teixeira <sergio@triggerise.org>
2023-08-30 12:02:23 +0100

    chores: fixes

--- phrike/unifi.nix
+++ phrike/unifi.nix
@@ -1,24 +1,8 @@
-{ config, pkgs, ... }:
-
-{
-  services.unifi = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.traefik.dynamicConfigOptions.http = {
-    routers.unifi = {
-      entryPoints = [ "https" ];
-      rule = "Host(`unifi.nathil.com`)";
-      service = "unifi";
-
-      tls = {
-        certResolver = "nathilcom";
-        domains = [{ main = "*.nathil.com"; }];
-      };
+{ config, pkgs, ... }: {
+  config.virtualisation.oci-containers.containers = {
+    unifi = {
+      image = "lscr.io/linuxserver/unifi-controller:latest";
+      ports = [ "8443:8443" "3478:3478" "8080:8080" "10001" ];
     };
-
-    services.unifi.loadBalancer.servers =
-      [{ url = "http://localhost:8443"; }];
   };
 }
