2a0e2df0f194dfa464d9f58865c168d853265f2d
Sergio Teixeira <sergio@triggerise.org>
2023-08-31 11:38:32 +0100

    chores: add steixeira user

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.steixeira = import ./helios/home.nix;
+            home-manager.users.steixeira = import ./users/steixeira.nix;
           }
         ];
       };
@@ -40,7 +40,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            #home-manager.users.steixeira = import ./helios/home.nix;
+            home-manager.users.steixeira = import ./users/steixeira.nix;
           }
         ];
       };
