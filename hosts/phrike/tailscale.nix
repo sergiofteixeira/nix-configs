bca7af6780777a2c4feda3be7d8b26afc0f44a50
Sergio Teixeira <sergiofpteixeira@gmail.com>
2023-09-02 17:38:03 +0100

    use tailscale key

--- hosts/phrike/tailscale.nix
+++ hosts/phrike/tailscale.nix
@@ -12,5 +12,26 @@
 
     # set this service as a oneshot job
     serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    age.secrets = {
+      tailscale = {
+        file = ../secrets/tailscale_key.age;
+      };
+    };
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      secret=$(cat "${config.age.secrets.tailscale.path}")
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey $secret --advertise-exit-node --advertise-routes=192.168.1.80/32,192.168.1.81/32 --reset
+    '';
   };
 }
