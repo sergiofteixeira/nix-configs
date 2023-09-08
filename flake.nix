e3ed3f1a8a8600a9584e35cb1c8b93f31f4a4c0a
Sergio Teixeira <sergio@triggerise.org>
2023-09-08 13:53:12 +0100

    add darwin configs

--- flake.nix
+++ flake.nix
@@ -8,9 +8,12 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
     nixinate.url = "github:matthewcroughan/nixinate";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, vscode-server, home-manager, agenix, nixinate, ... }: {
+  outputs = { self, nixpkgs, vscode-server, home-manager, agenix, nixinate, darwin, ... }: {
 
     apps = nixinate.nixinate.x86_64-linux self;
     nixosConfigurations = {
@@ -75,5 +78,23 @@
       };
 
     };
+
+    # darwin configs
+    darwinConfigurations.m1pro = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      modules = [
+        ./hosts/m1pro/darwin.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.steixeira.imports = [ ./hosts/m1pro/home.nix ];
+          };
+        }
+      ];
+    };
+
   };
 }
---
+++ hosts/m1pro/home.nix
@@ -0,0 +1,125 @@
+{ pkgs, ... }: {
+  home.stateVersion = "22.11";
+  home.packages = with pkgs; [
+    docker
+    docker-compose
+    kubectl
+    kubectx
+    kubernetes-helm
+    kubetail
+    ripgrep
+    fd
+    jq
+    curl
+    less
+    awscli2
+    nixfmt
+    watch
+    terraform
+    terraform-ls
+    rustup
+    go
+    tree-sitter
+    htop
+    unzip
+    nix-prefetch-scripts
+    go
+    gopls
+    nodejs
+  ];
+
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICLOLOR = 1;
+    EDITOR = "nvim";
+  };
+
+  home.file.".config/nvim".recursive = true;
+  home.file.".config/nvim".source = pkgs.fetchFromGitHub {
+    owner = "sergiofteixeira";
+    repo = "nvim";
+    rev = "772e840bb4aefcf8c5743e0d947ce69c172d4319";
+    sha256 = "sha256-1cXsZ9+5lwMTsc7Rh5KMGBwz55iHSAuxIOb/96JmpU8=";
+  };
+
+  programs.bat.enable = true;
+  programs.bat.config.theme = "TwoDark";
+
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
+
+  programs.exa.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Sergio Teixeira";
+    userEmail = "sergiofpteixeira@gmail.com";
+    aliases = {
+      prettylog =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      root = "rev-parse --show-toplevel";
+    };
+    extraConfig = {
+      color.ui = true;
+      github.user = "sergiofteixeira";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    enableAutosuggestions = true;
+    shellAliases = {
+      ls = "ls --color=auto -F";
+      nixswitch = "darwin-rebuild switch --flake ~/darwin-nix/.#m1pro";
+      vim = "nvim";
+      vi = "nvim";
+      k = "kubectl";
+      kx = "kubectx";
+      po = "kubectl get pod";
+      gc = "git checkout";
+      gs = "git status";
+    };
+
+  };
+
+  programs.tmux = {
+    enable = true;
+    terminal = "xterm-256color";
+    shortcut = "l";
+    secureSocket = false;
+
+    extraConfig = ''
+      unbind C-b
+      set -g prefix C-t
+      set -ga terminal-overrides ",*256col*:Tc"
+      bind -n C-k send-keys "clear"\; send-keys "Enter"
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    extraConfig = builtins.readFile ./kitty.conf;
+  };
+
+  programs.zoxide = {
+    enable = true;
+  };
+
+}
+
