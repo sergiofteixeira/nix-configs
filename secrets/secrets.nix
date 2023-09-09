3908bf8018a5f9d486f64085ef35308bd217f73e
Sergio Teixeira <sergio@triggerise.org>
2023-09-09 16:16:46 +0100

    add m1pro pub key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,8 +4,9 @@ let
   system-helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEsESHtTy5+eDFgiGJodDGLlEg4YFZFwhqMJjTrawcm root@nixos";
 
   system-phrike = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8tYvNe+s15uh8Sg03g8cCDCjCtfc/UDeNuWLb7qZ/l root@nixos";
+  m1pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP3Kyi4yJ9sMHfNrLalUH4m1asVxEaqXnqgX7LP1qz6+";
 in
 {
-  "cloudflare_token.age".publicKeys = [ steixeira system-helios system-phrike ];
-  "tailscale_key.age".publicKeys = [ steixeira system-helios system-phrike ];
+  "cloudflare_token.age".publicKeys = [ steixeira system-helios system-phrike m1pro ];
+  "tailscale_key.age".publicKeys = [ steixeira system-helios system-phrike m1pro ];
 }
