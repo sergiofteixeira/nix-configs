85959ecc21ea69c23db5dd871df8686d6e1669ea
Sergio Teixeira <sergio@triggerise.org>
2024-02-15 17:35:16 +0000

    enable jellyfin and lidarr on phrike

---
+++ hosts/phrike/lidarr.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  services.lidarr = {
+    enable = true;
+    openFirewall = true;
+    group = "wheel";
+    user = "steixeira";
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    routers.bazarr = {
+      entryPoints = [ "https" ];
+      rule = "Host(`lidarr.sergioteixeira.xyz`)";
+      service = "lidarrr";
+
+      tls = {
+        certResolver = "sergioteixeiraxyz";
+        domains = [{ main = "*.sergioteixeira.xyz"; }];
+      };
+    };
+
+    services.bazarr.loadBalancer.servers = [{ url = "http://localhost:8686"; }];
+  };
+}
+
+
