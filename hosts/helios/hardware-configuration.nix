a69c2685c1a77b3997b3575b2642ebb394cb10a4
Sergio Teixeira <sergiofpteixeira@gmail.com>
2024-02-27 18:31:25 +0000

    use zen kernel

--- hosts/helios/hardware-configuration.nix
+++ hosts/helios/hardware-configuration.nix
@@ -10,7 +10,7 @@
     [ "xhci_pci" "thunderbolt" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
