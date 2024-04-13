35581e05a21408d924a0f344b7e16ee0a733dd9a
Sergio Teixeira <sergiofpteixeira@gmail.com>
2024-04-13 15:19:27 +0100

    fix stuff

--- users/steixeira.nix
+++ users/steixeira.nix
@@ -3,6 +3,22 @@
 {
   home.username = "steixeira";
 
+  programs.chromium = {
+    enable = true;
+    commandLineArgs = [ "--force-device-scale-factor=2 --force-dark-mode" ];
+  };
+
+  programs.kitty = {
+    enable = true;
+    extraConfig = builtins.readFile ./configs/kitty/kitty.conf;
+  };
+
+  xsession.pointerCursor = {
+    name = "macOS-BigSur";
+    package = pkgs.apple-cursor;
+    size = 48;
+  };
+
   programs.git = {
     enable = true;
     userName = "Sergio Teixeira";
