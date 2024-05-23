d2b36545d06e3ab5a819f4ede59b1f20eef69c15
Sergio Teixeira <sergiofpteixeira@gmail.com>
2024-05-23 17:00:20 +0100

    loki overlay

--- hosts/phrike/loki.nix
+++ hosts/phrike/loki.nix
@@ -1,24 +1,11 @@
-{ pkgs, config, ... }:
-let
-  headscale = pkgs.headscale.override {
-    buildGoModule =
-      args:
-      pkgs.buildGoModule (
-        args
-        // rec {
-          version = "0.23.0-alpha5";
-          src = pkgs.fetchFromGitHub {
-            owner = "juanfont";
-            repo = "headscale";
-            rev = "v${version}";
-            hash = "sha256-BMrbYvxNAUs5vK7zCevSKDnB2npWZQpAtxoePXi5r40=";
-          };
-          vendorHash = "sha256-Yb5WaN0abPLZ4mPnuJGZoj6EMfoZjaZZ0f344KWva3o=";
-        }
-      );
-  };
-in
+{ config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ headscale ];
+  nixpkgs.overlays = [ (import ../../overlays/loki.nix) ];
+
+  # Configure Loki service
+  services.loki = {
+    enable = true;
+    package = pkgs.grafana-loki;
+  };
 }
