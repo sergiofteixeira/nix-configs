ea02513cb8ca7a070eee8750b6f0e46c32d3ae15
Sergio Teixeira <sergio@triggerise.org>
2024-09-03 09:50:14 +0100

    allow other systems to get api key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 let
   steixeira = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICySDx70VKoXhwoQbGGx1FpZsqWMhJxcOipc76eFztVZ sergio@triggerise.org";
-
   system-phrike = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8tYvNe+s15uh8Sg03g8cCDCjCtfc/UDeNuWLb7qZ/l root@nixos";
   m1pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP3Kyi4yJ9sMHfNrLalUH4m1asVxEaqXnqgX7LP1qz6+";
 in
@@ -12,6 +11,8 @@ in
   ];
   "anthropic_api_key.age".publicKeys = [
     steixeira
+    system-phrike
+    m1pro
   ];
   "tailscale_key.age".publicKeys = [
     steixeira
