84a43c9299556d967b7e415b4e012c8c2ead6aa6
Sergio Teixeira <sergio@triggerise.org>
2024-12-19 10:18:00 +0000

    fix
    Signed-off-by: Sergio Teixeira <sergio@triggerise.org>

--- hosts/phrike/configuration.nix
+++ hosts/phrike/configuration.nix
@@ -30,6 +30,9 @@
   nix.settings.trusted-users = [ "steixeira" ];
   nix.gc.automatic = true;
   nix.gc.options = "--delete-older-than 7d";
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-wrapped-6.0.36"
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
