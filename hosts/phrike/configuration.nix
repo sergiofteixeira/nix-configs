f56c8a0378f4320c921e019270c9929c32df4f37
Sergio Teixeira <sergiofpteixeira@gmail.com>
2025-01-03 10:06:22 +0000

    adds insecure packages

--- hosts/phrike/configuration.nix
+++ hosts/phrike/configuration.nix
@@ -51,7 +51,6 @@
     LC_TELEPHONE = "pt_PT.UTF-8";
     LC_TIME = "pt_PT.UTF-8";
   };
-
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
@@ -103,6 +102,9 @@
 
   nixpkgs.config.permittedInsecurePackages = [
     "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-wrapped-6.0.428"
+    "dotnet-sdk-6.0.428"
   ];
 
   environment.systemPackages = with pkgs; [
