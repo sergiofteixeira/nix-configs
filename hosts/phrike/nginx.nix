e9ff57a42051e2fdfe8dad6f4e5ce6f4b7148a01
Sergio Teixeira <sergiofpteixeira@gmail.com>
2025-06-26 10:27:37 +0100

    immich fixes

--- hosts/phrike/nginx.nix
+++ hosts/phrike/nginx.nix
@@ -85,8 +85,18 @@ in
         useACMEHost = acmeHost;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://localhost:2283";
+          proxyPass = " http://${config.services.immich.host}:${toString config.services.immich.port}";
+          proxyWebsockets = true;
         };
+        extraConfig = ''
+          # Allow large file uploads
+          client_max_body_size 50000M;
+
+          # Configure timeout
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
+          send_timeout       600s;
+        '';
       };
     };
   };
