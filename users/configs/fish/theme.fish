f6101f3e3c4523930d322cc4e8a379f714660262
Sergio Teixeira <sergio@triggerise.org>
2025-08-08 10:23:44 +0100

    updates
    Signed-off-by: Sergio Teixeira <sergio@triggerise.org>

--- users/configs/fish/theme.fish
+++ users/configs/fish/theme.fish
@@ -1,28 +1,51 @@
-set -l foreground DCD7BA normal
-set -l selection 2D4F67 brcyan
-set -l comment 727169 brblack
-set -l red C34043 red
-set -l orange FF9E64 brred
-set -l yellow C0A36E yellow
-set -l green 76946A green
-set -l purple 957FB8 magenta
-set -l cyan 7AA89F cyan
-set -l pink D27E99 brmagenta
+set -l foreground ebdbb2 normal
+set -l command 83a598
+set -l param d3869b
+set -l keyword fb4934
+set -l quote b8bb26
+set -l redirection d3869b
+set -l end fabd2f
+set -l comment 928374 brblack
+set -l error fb4934 red
+set -l gray 928374
+set -l selection 665c54 brcyan
+set -l option b8bb26
+set -l operator d3869b
+set -l escape d3869b
+set -l autosuggestion 928374
+set -l cancel fb4934
+set -l cwd fabd2f
+set -l user 8ec07c
+set -l host 83a598
+set -l host_remote b8bb26
+set -l status fb4934
+
+set -l pager_progress 928374
+set -l pager_prefix d3869b
+set -l pager_completion ebdbb2
+set -l pager_description 928374
+
 set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
+set -g fish_color_command $command
+set -g fish_color_param $param set -g fish_color_keyword $keyword set -g fish_color_quote $quote set -g fish_color_redirection $redirection
+set -g fish_color_end $end
 set -g fish_color_comment $comment
+set -g fish_color_error $error
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
+set -g fish_color_option $option
+set -g fish_color_operator $operator
+set -g fish_color_escape $escape
+set -g fish_color_autosuggestion $autosuggestion
+set -g fish_color_cancel $cancel
+set -g fish_color_cwd $cwd
+set -g fish_color_user $user
+set -g fish_color_host $host
+set -g fish_color_host_remote $host_remote
+set -g fish_color_status $status
+
+set -g fish_pager_color_progress $pager_progress
+set -g fish_pager_color_prefix $pager_prefix
+set -g fish_pager_color_completion $pager_completion
+set -g fish_pager_color_description $pager_description
+
