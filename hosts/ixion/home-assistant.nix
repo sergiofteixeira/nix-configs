5b62ba22366f99ac4091242dd7a3b23e7ba9e9a1
Sergio Teixeira <sergiofpteixeira@gmail.com>
2026-05-15 13:34:33 +0100

    add component to ha

--- hosts/ixion/home-assistant.nix
+++ hosts/ixion/home-assistant.nix
@@ -6,8 +6,13 @@
     openFirewall = true;
     extraComponents = [
       "apple_tv"
+      "bluetooth"
+      "spotify"
+      "jellyfin"
       "ipma"
       "tplink"
+      "wake_on_lan"
+      "mobile_app"
     ];
     config = {
       default_config = { };
@@ -16,6 +21,7 @@
         trusted_proxies = [ "127.0.0.1" ];
         use_x_forwarded_for = true;
       };
+      automation = "!include automations.yaml";
     };
   };
 }
