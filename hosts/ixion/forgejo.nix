8bc0d8bff1e9b5c60d718f92c24fa59b1647b6c1
Sergio Teixeira <sergiofpteixeira@gmail.com>
2026-05-26 14:40:17 +0100

    add forgejo custom logo

--- hosts/ixion/forgejo.nix
+++ hosts/ixion/forgejo.nix
@@ -28,9 +28,14 @@
     "d /var/lib/forgejo/custom/public/assets 0755 forgejo forgejo - -"
     "d /var/lib/forgejo/custom/public/assets/css 0755 forgejo forgejo - -"
     "d /var/lib/forgejo/custom/public/assets/img 0755 forgejo forgejo - -"
+    "d /var/lib/forgejo/custom/templates 0755 forgejo forgejo - -"
+    "d /var/lib/forgejo/custom/templates/custom 0755 forgejo forgejo - -"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite.css - - - - ${./forgejo-themes/theme-anthracite.css}"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite-dark.css - - - - ${./forgejo-themes/theme-anthracite-dark.css}"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite-light.css - - - - ${./forgejo-themes/theme-anthracite-light.css}"
+    "L+ /var/lib/forgejo/custom/public/assets/css/custom-logo.css - - - - ${./forgejo-themes/custom-logo.css}"
+    "L+ /var/lib/forgejo/custom/public/assets/img/logo.svg - - - - ${./forgejo-themes/logo.svg}"
     "L+ /var/lib/forgejo/custom/public/assets/img/logo.png - - - - ${./forgejo-themes/logo.png}"
+    "L+ /var/lib/forgejo/custom/templates/custom/header.tmpl - - - - ${./forgejo-themes/header.tmpl}"
   ];
 }
