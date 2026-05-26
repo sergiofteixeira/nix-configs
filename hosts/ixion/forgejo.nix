098f2e17f74ff0ea284baec56932d5aa86eab421
Sergio Teixeira <sergiofpteixeira@gmail.com>
2026-05-26 14:36:43 +0100

    add forgejo custom logo

--- hosts/ixion/forgejo.nix
+++ hosts/ixion/forgejo.nix
@@ -27,8 +27,10 @@
     "d /var/lib/forgejo/custom/public 0755 forgejo forgejo - -"
     "d /var/lib/forgejo/custom/public/assets 0755 forgejo forgejo - -"
     "d /var/lib/forgejo/custom/public/assets/css 0755 forgejo forgejo - -"
+    "d /var/lib/forgejo/custom/public/assets/img 0755 forgejo forgejo - -"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite.css - - - - ${./forgejo-themes/theme-anthracite.css}"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite-dark.css - - - - ${./forgejo-themes/theme-anthracite-dark.css}"
     "L+ /var/lib/forgejo/custom/public/assets/css/theme-anthracite-light.css - - - - ${./forgejo-themes/theme-anthracite-light.css}"
+    "L+ /var/lib/forgejo/custom/public/assets/img/logo.png - - - - ${./forgejo-themes/logo.png}"
   ];
 }
